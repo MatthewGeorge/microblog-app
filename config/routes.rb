b6cd957d0bb350f5f10ced341cb1d6d20ed2d75e
Matt George <matt.george14@gmail.com>
2014-04-01 00:46:47 +0000

    add microblog

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 MicroblogApp::Application.routes.draw do
+  resources :microblogs
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
